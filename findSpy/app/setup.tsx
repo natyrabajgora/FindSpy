d3efe5ba6a340d906e9c640b2d7180ea600af943
vesahadergjonaj <vesa.hadergjonaj@gmail.com>
2025-10-18 22:12:18 +0200

    opsionet

--- findSpy/app/setup.tsx
+++ findSpy/app/setup.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 import { SafeAreaView, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function SetupScreen() {
-  return (
+  return ( /*gjdo sen qe bohet mrena return ka me u shfaq ne screen ne form tu UI*/
     <SafeAreaView style={styles.safe}>
-      <View style={styles.container}>
+      <View style={styles.container}>  {/* si container munen me e mar si div */}
         <Text style={styles.title}>Game Setup</Text>
 
         <View style={styles.card}>
@@ -15,6 +15,31 @@ export default function SetupScreen() {
             <Text style={styles.value}>4</Text>
             <Text style={styles.step}>+</Text>
           </View>
+        </View>
+
+        <View style={styles.card}>
+            <Text style={styles.label}>Number of Spys</Text>
+            <View style={styles.row}>
+                <Text style={styles.step}>-</Text>
+                <Text style={styles.step}>2</Text>
+                <Text style={styles.step}>+</Text>
+            </View>
+        </View>
+
+         <View style={styles.card}>
+            <Text style={styles.label}>Duration</Text>
+            <View style={styles.row}>
+                <Text style={styles.step}>-</Text>
+                <Text style={styles.step}>5</Text>
+                <Text style={styles.step}>+</Text>
+            </View>
+        </View>
+
+         <View style={styles.card}>
+            <Text style={styles.label}>Category</Text>
+            <View style={styles.row}>
+                <Text style={styles.step}>chose</Text>
+            </View>
         </View>
 
         <TouchableOpacity style={styles.startBtn}>
@@ -27,17 +52,23 @@ export default function SetupScreen() {
 
 const styles = StyleSheet.create({
   safe: { flex: 1 },
-  container: { flex: 1, padding: 16, gap: 5 },
+  container: {  flexDirection: 'column', justifyContent: 'center',flex: 1, padding: 30, gap: 20 },
   title: { fontSize: 20, fontWeight: '700', textAlign: 'center', marginVertical: 8 },
 
   card: {
     padding: 14,
     borderWidth: 1,
     borderRadius: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  label: { fontSize: 14, marginBottom: 8 },
-  row: { flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' },
-  step: { fontSize: 18, paddingHorizontal: 12, paddingVertical: 4 }, // “-” dhe “+” placeholder
+  label: { fontSize: 14,},
+  
+  row: { flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' },
+                
+  step: { fontSize: 18, paddingHorizontal: 4, paddingVertical: 12 }, // “-” dhe “+” placeholder
+  
   value: { fontSize: 16 },
 
   startBtn: {
